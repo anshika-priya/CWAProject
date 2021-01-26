c4ba5f600e20c6a3ed3b9eca3926d56f1f8b6d99
anshika-priya <62823804+anshika-priya@users.noreply.github.com>
2021-01-26 11:09:02 +0530

    Delete PWA project/bad-news-master directory

--- PWA project/bad-news-master/newsApi.js
+++
@@ -1,5 +0,0 @@
-const apiKey = '7df392927a2e43d1881c257f66c467ec';
-const topHeadlinesUrl =
-  'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
-
-export { apiKey, topHeadlinesUrl };
